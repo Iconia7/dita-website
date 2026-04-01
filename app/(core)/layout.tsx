ceef0bea3d116fd6055dbe466b513ea453c664fc
victormusembi11 <victor.musembi11@gmail.com>
2026-04-01 06:52:30 +0300

    feat: move AppCTA component to Home page for improved visibility

--- app/(core)/layout.tsx
+++ app/(core)/layout.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/main-navbar";
 import Footer from "@/components/footer";
-import AppCTA from "@/components/app-cta";
 
 export default function CoreLayout({
   children,
@@ -11,7 +10,6 @@ export default function CoreLayout({
     <>
       <Navbar />
       <main className="flex-1">{children}</main>
-      <AppCTA />
       <Footer />
     </>
   );
